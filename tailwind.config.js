1efd4764f64c63a55e7d4c909b3ec8c92a9306b8
arvinogtya <vinoadhiarsa77@gmail.com>
2024-09-12 17:51:16 +0700

    10+

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
-  darkMode: 'class',
+  content: ['index.html'],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "1024px"
+    container: {
+      center: true,
+      padding: '20px',
     },
     extend: {
       colors: {
-        primaryColor: '#fff',
-        primaryColorLight: "hsl(209, 74%, 45%)",
-        sectionColor: 'hsl(209, 100%, 97%)',
-        secondaryColor: '#FFCC00',
-        paragraphColor: '#c0c0c0',      
-        whiteColor: '#fff',
-        blackColor: "#000",
-        greenColor: "#007936",
-        redColor: "#cc3433",
-        darkColor: 'hsl(216, 100%, 4%)',
-        darkColorLight: "hsl(211, 100%, 12%)",
-        textColor: "#fff",
+        primary: '#7dd3fc',
+        drak: '#030712',
+        secondary: '#64748b',
+        main: '#030712',
+        font: '#fdf4ff',
       },
-      keyframes: {
-        move: {
-          "50%": {transform: 'translateY(-1rem)'}
-        }
+      screens: {
+        '2xl': '1320px',
       },
-      animation: {
-        'movingY': 'move 2s linear infinite'
-      }
     },
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '1.5rem'
-      }
-    },
-    fontFamily: {
-      oswald: ['Oswald', 'sans-serif'],
-      dmsans: ['DM Sans', 'sans-serif']
-    }
   },
   plugins: [],
 }
+
